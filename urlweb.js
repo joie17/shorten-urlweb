374ce8d1f8b4fa58654d7fbbb798fba4301a3604
KUKROLIA JAINIL <jainilkukrolia@gmail.com>
2025-06-24 21:25:23 +0530

    Fix: listen on 0.0.0.0 and process.env.PORT

--- urlweb.js
+++ urlweb.js
@@ -7,7 +7,13 @@ import crypto from "crypto";
 import { writeFile } from "fs/promises";
 import { log } from "console";
 
-const PORT = 3002;
+// const PORT = 3002;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
 const DATA_FILE = path.join("data", "links.json");
 const serverFile = async (res, filePath, contentType) => {
   try {
